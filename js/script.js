8e60c03dfbd41b01d936faee59439dd25fb08cfd
indigitals <96959296+indigitals@users.noreply.github.com>
2022-01-09 16:24:13 -0600

    play + pause button functionaility

--- js/script.js
+++ js/script.js
@@ -2,3 +2,51 @@ function play() {
    document.getElementById("audio").play();
   }
 
+function pause() {
+   document.getElementById("audio").pause();
+}
+
+var state = {
+   musicPlaying: false,
+   firstClick: false,
+   videos: [{        
+      url: "media/NOT  Who Do I Trust Official Audio.mp3",
+      title: "Who Do I trust $NOT"
+      }]
+}
+
+document.getElementById("audio").src = state.videos[Math.floor(Math.random() * state.videos.length)].url
+
+
+function musicSwitcher() {
+   document.querySelector('.stateSwitch').addEventListener('click', stateSwitch)
+
+   function stateSwitch() {
+       if (state.musicPlaying) {
+           state.musicPlaying = false;
+           document.querySelector('.pause').style.display = 'none';
+           document.querySelector('.play').style.display = 'inline-block';
+           pause();
+       } else {
+           state.musicPlaying = true;
+           document.querySelector('.play').style.display = 'none';
+           document.querySelector('.pause').style.display = 'inline-block';
+           play();
+       }
+   }
+}
+
+function firstClick() {
+   if (!state.firstClick) {
+       state.firstClick = true;
+       init();
+   };
+};
+
+function init(){
+   document.querySelector('.play').style.display = 'none';
+   document.querySelector('.pause').style.display = 'inline-block';
+   state.musicPlaying = true;
+   musicSwitcher();
+}
+document.addEventListener('click', firstClick);
